24c7a8a299e3fd4eeeddfbad7c679ce375b97343
DragN0007 <115123493+DragN0007@users.noreply.github.com>
2026-03-26 02:14:03 -0500

    -init

--- src/main/java/com/dragn0007/dragnloextras/util/ScrapsExtrasClientConfig.java
+++ src/main/java/com/dragn0007/dragnloextras/util/ScrapsExtrasClientConfig.java
@@ -11,9 +11,7 @@ public class ScrapsExtrasClientConfig {
     static {
         BUILDER.push("Client");
         BUILDER.pop();
-
         RENDER_DIRT = BUILDER.define("Render Animal Dirt Layer", true);
-
         SPEC = BUILDER.build();
     }
 }
